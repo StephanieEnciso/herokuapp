546855fe806d49f7bc518dac9a8a11e94a276dce
daveKidd <david.kidd@lambdaschool.com>
2021-02-11 11:04:52 -0700

    first push

---
+++ index.js
@@ -0,0 +1,34 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+
+
+const app = express()
+const port = process.env.PORT || 9000
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/*",(_,res)=>{
+    res.json({data: "The API LIVES!!!!"})
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is alive on port ${port}`)
+})
+
+
+
+
+
+
+// console.log("It's alive!")
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(process.env.USERNAME)
+// //.USER on macs!!!
+// console.log(process.env.PORT)
+// console.log(dotenv)
+// console.log(dotenv.PORT)
+// console.log(process.env.FOOD)
+
